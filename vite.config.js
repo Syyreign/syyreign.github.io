acdf54dba4542d1abcc220910e8f25e07d0e519c
Syyreign <spencerehart@hotmail.com>
2024-02-18 21:47:17 -0800

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-export default {
-    
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    base: '/SpEllHart/',
+    plugins: [vue()]
+})
